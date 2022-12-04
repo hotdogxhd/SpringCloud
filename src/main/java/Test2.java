2bc43c17d4b2bec9767245d158124015c147ccaa
xhd <1922434@qq.com>
2022-12-04 14:38:40 +0800

    14.38 xhd 4 commit! hot-key

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("2222");
+        System.out.println("hot-key  冲突修改！！！");
     }
 }
--- src/main/java/Test3.java
+++ src/main/java/Test3.java
@@ -1,3 +1,5 @@
 public class Test3 {
-
+    public static void main(String[] args) {
+        System.out.println("hot-key  冲突修改！！！");
+    }
 }
